e796589e5f9a21ec7f8540ba69532b01e9dd3f18
Naoyuki Kumagai <kumagai@namaka>
2023-09-13 14:12:37 +0900

    Refactor action 'albums#index' for future filter chaining

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,8 +2,8 @@ class AlbumsController < ApplicationController
 
   def index
     @genre = Genre.find_by_name(params[:genre])
-    @albums = @genre.filter_albums(
-      Album.includes(:artist, :tracks).local
-    ).sort
+    @albums = Album.includes(:artist, :tracks).local.then { |albums|
+      @genre.filter_albums(albums)
+    }.sort
   end
 end
