f78a2ee5c0cc179714db15d867274915e175996d
Naoyuki Kumagai <kumagai@namaka>
2023-09-13 11:40:29 +0900

    Modify 'albums#index' filterable by Genre

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,9 @@
 class AlbumsController < ApplicationController
 
   def index
-    @albums = Album.local.sort
+    genre = Genre.find_by_name(params[:genre])
+    @albums = genre.filter_albums(
+      Album.includes(:artist, :tracks).local
+    ).sort
   end
 end
