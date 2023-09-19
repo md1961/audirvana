c24aae5feabeccbe5aedfee5e838795fc2fc47b6
Naoyuki Kumagai <kumagai@namaka>
2023-09-19 14:31:52 +0900

    Add value object Albums::Year

---
+++ app/models/albums/year.rb
@@ -0,0 +1,25 @@
+class Albums::Year
+
+  def initialize(album)
+    raise ArgumentError, "Argument must be an Album" unless album.is_a?(Album)
+
+    @year_from_album, @years_from_tracks = album.then { |album|
+      [
+        album.release_year,
+        album.tracks.map(&:year).uniq.compact.sort
+      ]
+    }
+  end
+
+  def strict?
+    [@year_from_album] == @years_from_tracks
+  end
+
+  def value
+    @year_from_album || @years_from_tracks.max
+  end
+
+  def values
+    (@years_from_tracks + [@year_from_album]).uniq.compact.sort
+  end
+end
